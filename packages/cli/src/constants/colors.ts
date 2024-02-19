a69d4c037ff2d9142191e3916ae30c0b056823c5
meluiz <me.luizfelipe@gmail.com>
2024-02-19 13:48:58 -0300

    feat(colors.ts): add color constants for gray and primary colors
    The `colors.ts` file was added to the `constants` directory in the `cli` package. This file exports an object that contains color constants for the gray and primary colors. These constants are used for styling purposes throughout the CLI application.

---
+++ packages/cli/src/constants/colors.ts
@@ -0,0 +1,46 @@
+export default {
+  gray: [
+    {
+      label: "Neutral",
+      value: "neutral",
+    },
+    {
+      label: "Mauve",
+      value: "mauve",
+    },
+    {
+      label: "Olive",
+      value: "olive",
+    },
+    {
+      label: "Sage",
+      value: "sage",
+    },
+    {
+      label: "Sand",
+      value: "sand",
+    },
+    {
+      label: "Slate",
+      value: "slate",
+    },
+  ],
+  primary: [
+    {
+      label: "Orange",
+      value: "orange",
+    },
+    {
+      label: "Zinc",
+      value: "zinc",
+    },
+    {
+      label: "Blue",
+      value: "blue",
+    },
+    {
+      label: "Green",
+      value: "green",
+    },
+  ],
+};
